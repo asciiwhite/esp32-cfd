f420779323ab427b88fc607729937558c4b82f9e
Christian Vix <christian_vix@mentor.com>
2019-02-27 13:14:56 +0100

    Removed obsolete starfield and refactored code for readability

--- Framework.ino
+++ Framework.ino
@@ -1,83 +1,70 @@
 #include "fluid.h"
 #include "fluidvisualizer.h"
-//#include "starfield.h"
 #include "infobar.h"
 
 #include <TFT_eSPI.h>
 
-const uint32_t FLUID_SCALE = 4;
-
 TFT_eSPI tft = TFT_eSPI();
-//Starfield stars = Starfield();
+
+InfoBar infoBar = InfoBar(tft);
+const uint32_t InfoBarHeight = 10;
+
+const uint32_t FLUID_SCALE = 4;
 Fluid fluid = Fluid(0.2f, 0.f, 0.0000001f);
 FluidVisualizer fluidviz = FluidVisualizer(tft, FLUID_SCALE);
-InfoBar infoBar = InfoBar(tft);
 
-const uint32_t InfoBarHeight = 10;
 const uint32_t MaxFrameTimeInMillis = 33;
 
-bool warpSpeed = false;
-uint32_t warpSpeedNextToggleInMillis = 2000;
-
-const uint32_t WallHeightInPixels = 30;
-
-float t = 0.f;
+float angleInRadians = 0.f;
+const float rotationDurationInSeconds = 10.0f;
+const float fadeDensityPerFrame = 1.5f;
 
 void setup() {
-  tft.init();
-  tft.setRotation(0);
-  tft.fillScreen(TFT_BLACK);
-  tft.setTextSize(1);
-  tft.setTextFont(1);
+    tft.init();
+    tft.setRotation(0);
+    tft.fillScreen(TFT_BLACK);
+    tft.setTextSize(1);
+    tft.setTextFont(1);
   
-  Serial.begin(115200);
+    Serial.begin(115200);
 
-  const uint32_t width = std::ceil(tft.width() / FLUID_SCALE);
-  const uint32_t height = std::ceil((tft.height() - InfoBarHeight) / FLUID_SCALE);
-  fluid.init(width, height);
-  fluidviz.init(fluid);
- // stars.init(1024);
+    const uint32_t width = std::ceil(tft.width() / FLUID_SCALE);
+    const uint32_t height = std::ceil((tft.height() - InfoBarHeight) / FLUID_SCALE);
+    fluid.init(width, height);
+    fluidviz.init(fluid);
+ }
+
+void addRotatingFluid() {
+    const auto cx = fluid.getWidth() * 0.5f;
+    const auto cy = fluid.getHeight() * 0.5f;
+    for (int i = -1; i <= 1; i++) {
+        for (int j = -1; j <= 1; j++) {
+            fluid.addDensity(cx + i, cy + j, random(50, 100));
+        }
+    }
+
+    const float vx = sin(angleInRadians) * 0.2f;
+    const float vy = cos(angleInRadians) * 0.2f;
+    fluid.addVelocity(cx, cy, vx, vy);
 }
 
 void loop() {
-  const long start = millis();
+    const long start = millis();
 
-/*  if (start > warpSpeedNextToggleInMillis)
-  {
-    warpSpeedNextToggleInMillis = random(2000, 8000) + start;
-    warpSpeed = !warpSpeed;
-  }
-  stars.draw(tft, warpSpeed, 0, InfoBarHeight, tft.width(), tft.height() - InfoBarHeight);
-*/
+    addRotatingFluid();
+    fluid.fadeDensity(fadeDensityPerFrame);
+    fluid.step();
 
-  const auto cx = fluid.getWidth() * 0.5f;
-  const auto cy = fluid.getHeight() * 0.5f;
-  for (int i = -1; i <= 1; i++) {
-      for (int j = -1; j <= 1; j++) {
-          fluid.addDensity(cx + i, cy + j, random(50, 100));
-      }
-  }
-  //for (int i = 0; i < 2; i++) {
-//      const float angle = noise(t) * TWO_PI * 2;
-//      PVector v = PVector.fromAngle(angle);
-      float vx = sin(t) * 0.2f;
-      float vy = cos(t) * 0.2f;
-      t += 0.04f;
-      fluid.addVelocity(cx, cy, vx, vy);
-  //}
+    const long processingTime = millis() - start;
 
-  fluid.step();
+    tft.setCursor(0, InfoBarHeight);
+    fluidviz.handleTouch();
+    fluidviz.draw();
 
-  const long processingTime = millis() - start;
-
-  tft.setCursor(0, InfoBarHeight);
-  fluidviz.handleTouch();
-  fluidviz.draw();
-
-  fluid.fadeDensity(1.5f);
-
-  const long frameTime = millis() - start;
-  infoBar.update(start, processingTime, frameTime);
-  if (frameTime < MaxFrameTimeInMillis)
+    const long frameTime = millis() - start;
+    infoBar.update(start, processingTime, frameTime);
+    if (frameTime < MaxFrameTimeInMillis)
     delay(MaxFrameTimeInMillis - frameTime);
+
+    angleInRadians += (static_cast<float>(frameTime) / 1000.f) * TWO_PI / rotationDurationInSeconds;
 }
